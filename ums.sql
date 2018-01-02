3cfac614b9b15bd0cf53e43254221db0bcd9631d
Delila Halilovic <delila.xd@gmail.com>
2018-01-02 22:58:10 +0100

    i baze

--- ums.sql
+++ ums.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Dec 31, 2017 at 12:07 PM
--- Server version: 5.7.14
--- PHP Version: 5.6.25
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jan 02, 2018 at 09:57 PM
+-- Server version: 5.7.19
+-- PHP Version: 5.6.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -26,11 +28,13 @@ SET time_zone = "+00:00";
 -- Table structure for table `ar_internal_metadata`
 --
 
-CREATE TABLE `ar_internal_metadata` (
+DROP TABLE IF EXISTS `ar_internal_metadata`;
+CREATE TABLE IF NOT EXISTS `ar_internal_metadata` (
   `key` varchar(255) NOT NULL,
   `value` varchar(255) DEFAULT NULL,
   `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`key`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -46,14 +50,16 @@ INSERT INTO `ar_internal_metadata` (`key`, `value`, `created_at`, `updated_at`) 
 -- Table structure for table `confirmations`
 --
 
-CREATE TABLE `confirmations` (
-  `id` bigint(20) NOT NULL,
+DROP TABLE IF EXISTS `confirmations`;
+CREATE TABLE IF NOT EXISTS `confirmations` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `svrha` varchar(255) DEFAULT NULL,
   `ime` varchar(255) DEFAULT NULL,
   `prezime` varchar(255) DEFAULT NULL,
   `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `confirmations`
@@ -61,7 +67,11 @@ CREATE TABLE `confirmations` (
 
 INSERT INTO `confirmations` (`id`, `svrha`, `ime`, `prezime`, `created_at`, `updated_at`) VALUES
 (1, 'znanje', 'amina', 'mah', '2017-12-01 00:00:00', '2017-12-27 00:00:00'),
-(2, 'zdravlje', 'delila', 'halilovic', '2017-12-01 00:00:00', '2017-12-27 00:00:00');
+(2, 'zdravlje', 'delila', 'halilovic', '2017-12-01 00:00:00', '2017-12-27 00:00:00'),
+(3, 'Regulisanje stipendije', 'Amina', 'Mahmutovic', '2018-01-02 15:38:28', '2018-01-02 15:38:28'),
+(5, 'Za isplatu', 'Huse', 'Fatkic', '2018-01-02 17:20:49', '2018-01-02 17:20:49'),
+(6, 'Za isplatu', 'Delila', 'Halilovic', '2018-01-02 17:30:55', '2018-01-02 17:30:55'),
+(8, 'Za regulisanje zdravstvenog osiguranja', 'Selsebil', 'Catic', '2018-01-02 18:03:58', '2018-01-02 18:03:58');
 
 -- --------------------------------------------------------
 
@@ -69,12 +79,14 @@ INSERT INTO `confirmations` (`id`, `svrha`, `ime`, `prezime`, `created_at`, `upd
 -- Table structure for table `job_advertisements`
 --
 
-CREATE TABLE `job_advertisements` (
-  `id` bigint(20) NOT NULL,
+DROP TABLE IF EXISTS `job_advertisements`;
+CREATE TABLE IF NOT EXISTS `job_advertisements` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `work_position` varchar(255) DEFAULT NULL,
   `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `job_advertisements`
@@ -91,8 +103,9 @@ INSERT INTO `job_advertisements` (`id`, `work_position`, `created_at`, `updated_
 -- Table structure for table `job_applications`
 --
 
-CREATE TABLE `job_applications` (
-  `id` bigint(20) NOT NULL,
+DROP TABLE IF EXISTS `job_applications`;
+CREATE TABLE IF NOT EXISTS `job_applications` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   `surname` varchar(255) DEFAULT NULL,
   `JBMG` int(11) DEFAULT NULL,
@@ -105,8 +118,10 @@ CREATE TABLE `job_applications` (
   `motivation_letter` text,
   `job_advertisements_id` bigint(20) DEFAULT NULL,
   `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_job_applications_on_job_advertisements_id` (`job_advertisements_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `job_applications`
@@ -122,8 +137,9 @@ INSERT INTO `job_applications` (`id`, `name`, `surname`, `JBMG`, `date_of_birth`
 -- Table structure for table `results`
 --
 
-CREATE TABLE `results` (
-  `id` bigint(20) NOT NULL,
+DROP TABLE IF EXISTS `results`;
+CREATE TABLE IF NOT EXISTS `results` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   `surname` varchar(255) DEFAULT NULL,
   `jmbg` int(11) DEFAULT NULL,
@@ -143,8 +159,10 @@ CREATE TABLE `results` (
   `points` float DEFAULT NULL,
   `mark` int(11) DEFAULT NULL,
   `subject_id` bigint(20) DEFAULT NULL,
-  `proba` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `proba` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_results_on_subject_id` (`subject_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -152,8 +170,10 @@ CREATE TABLE `results` (
 -- Table structure for table `schema_migrations`
 --
 
-CREATE TABLE `schema_migrations` (
-  `version` varchar(255) NOT NULL
+DROP TABLE IF EXISTS `schema_migrations`;
+CREATE TABLE IF NOT EXISTS `schema_migrations` (
+  `version` varchar(255) NOT NULL,
+  PRIMARY KEY (`version`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -185,16 +205,18 @@ INSERT INTO `schema_migrations` (`version`) VALUES
 -- Table structure for table `subjects`
 --
 
-CREATE TABLE `subjects` (
-  `id` bigint(20) NOT NULL,
+DROP TABLE IF EXISTS `subjects`;
+CREATE TABLE IF NOT EXISTS `subjects` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) DEFAULT NULL,
   `ECTS` int(11) DEFAULT NULL,
   `cycle` int(11) DEFAULT NULL,
   `semester` int(11) DEFAULT NULL,
   `subject_type` varchar(255) DEFAULT NULL,
   `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `subjects`
@@ -205,83 +227,6 @@ INSERT INTO `subjects` (`id`, `title`, `ECTS`, `cycle`, `semester`, `subject_typ
 (2, 'TP', 6, 1, 1, 'Obavezni', '2017-12-28 23:28:59', '2017-12-28 23:28:59'),
 (3, 'OR', 5, 1, 1, 'Obavezni', '2017-12-28 23:33:04', '2017-12-28 23:33:04');
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `ar_internal_metadata`
---
-ALTER TABLE `ar_internal_metadata`
-  ADD PRIMARY KEY (`key`);
-
---
--- Indexes for table `confirmations`
---
-ALTER TABLE `confirmations`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `job_advertisements`
---
-ALTER TABLE `job_advertisements`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `job_applications`
---
-ALTER TABLE `job_applications`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `index_job_applications_on_job_advertisements_id` (`job_advertisements_id`);
-
---
--- Indexes for table `results`
---
-ALTER TABLE `results`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `index_results_on_subject_id` (`subject_id`);
-
---
--- Indexes for table `schema_migrations`
---
-ALTER TABLE `schema_migrations`
-  ADD PRIMARY KEY (`version`);
-
---
--- Indexes for table `subjects`
---
-ALTER TABLE `subjects`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `confirmations`
---
-ALTER TABLE `confirmations`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `job_advertisements`
---
-ALTER TABLE `job_advertisements`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
---
--- AUTO_INCREMENT for table `job_applications`
---
-ALTER TABLE `job_applications`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
---
--- AUTO_INCREMENT for table `results`
---
-ALTER TABLE `results`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
---
--- AUTO_INCREMENT for table `subjects`
---
-ALTER TABLE `subjects`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- Constraints for dumped tables
 --
@@ -297,6 +242,7 @@ ALTER TABLE `job_applications`
 --
 ALTER TABLE `results`
   ADD CONSTRAINT `fk_rails_a6f81d1959` FOREIGN KEY (`subject_id`) REFERENCES `subjects` (`id`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- ums2.sql
+++ ums2.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Dec 31, 2017 at 12:02 PM
--- Server version: 5.7.14
--- PHP Version: 5.6.25
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jan 02, 2018 at 09:57 PM
+-- Server version: 5.7.19
+-- PHP Version: 5.6.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -26,11 +28,13 @@ SET time_zone = "+00:00";
 -- Table structure for table `ar_internal_metadata`
 --
 
-CREATE TABLE `ar_internal_metadata` (
+DROP TABLE IF EXISTS `ar_internal_metadata`;
+CREATE TABLE IF NOT EXISTS `ar_internal_metadata` (
   `key` varchar(255) NOT NULL,
   `value` varchar(255) DEFAULT NULL,
   `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`key`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -46,12 +50,15 @@ INSERT INTO `ar_internal_metadata` (`key`, `value`, `created_at`, `updated_at`) 
 -- Table structure for table `exams`
 --
 
-CREATE TABLE `exams` (
-  `id` bigint(20) NOT NULL,
+DROP TABLE IF EXISTS `exams`;
+CREATE TABLE IF NOT EXISTS `exams` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `exam_time` datetime DEFAULT NULL,
   `exam_place` varchar(255) DEFAULT NULL,
-  `subject_id` bigint(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `subject_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_exams_on_subject_id` (`subject_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `exams`
@@ -66,12 +73,16 @@ INSERT INTO `exams` (`id`, `exam_time`, `exam_place`, `subject_id`) VALUES
 -- Table structure for table `exam_applications`
 --
 
-CREATE TABLE `exam_applications` (
-  `id` bigint(20) NOT NULL,
+DROP TABLE IF EXISTS `exam_applications`;
+CREATE TABLE IF NOT EXISTS `exam_applications` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `apply` tinyint(1) DEFAULT NULL,
   `exam_id` bigint(20) DEFAULT NULL,
-  `student_id` bigint(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `student_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_exam_applications_on_exam_id` (`exam_id`),
+  KEY `index_exam_applications_on_student_id` (`student_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `exam_applications`
@@ -86,8 +97,10 @@ INSERT INTO `exam_applications` (`id`, `apply`, `exam_id`, `student_id`) VALUES
 -- Table structure for table `schema_migrations`
 --
 
-CREATE TABLE `schema_migrations` (
-  `version` varchar(255) NOT NULL
+DROP TABLE IF EXISTS `schema_migrations`;
+CREATE TABLE IF NOT EXISTS `schema_migrations` (
+  `version` varchar(255) NOT NULL,
+  PRIMARY KEY (`version`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -114,7 +127,8 @@ INSERT INTO `schema_migrations` (`version`) VALUES
 ('20171228221018'),
 ('20171228230540'),
 ('20171228231100'),
-('20171228231624');
+('20171228231624'),
+('20180102170853');
 
 -- --------------------------------------------------------
 
@@ -122,20 +136,24 @@ INSERT INTO `schema_migrations` (`version`) VALUES
 -- Table structure for table `students`
 --
 
-CREATE TABLE `students` (
-  `id` bigint(20) NOT NULL,
+DROP TABLE IF EXISTS `students`;
+CREATE TABLE IF NOT EXISTS `students` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `index` int(11) DEFAULT NULL,
   `ects` int(11) DEFAULT NULL,
   `year` int(11) DEFAULT NULL,
-  `user_id` bigint(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `user_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_students_on_user_id` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `students`
 --
 
 INSERT INTO `students` (`id`, `index`, `ects`, `year`, `user_id`) VALUES
-(1, 17043, 20, 2, 1);
+(1, 17043, 20, 2, 1),
+(2, 12345, 50, 4, 3);
 
 -- --------------------------------------------------------
 
@@ -143,14 +161,16 @@ INSERT INTO `students` (`id`, `index`, `ects`, `year`, `user_id`) VALUES
 -- Table structure for table `subjects`
 --
 
-CREATE TABLE `subjects` (
-  `id` bigint(20) NOT NULL,
+DROP TABLE IF EXISTS `subjects`;
+CREATE TABLE IF NOT EXISTS `subjects` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) DEFAULT NULL,
   `ECTS` int(11) DEFAULT NULL,
   `cycle` int(11) DEFAULT NULL,
   `semester` int(11) DEFAULT NULL,
-  `subject_type` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `subject_type` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `subjects`
@@ -165,8 +185,9 @@ INSERT INTO `subjects` (`id`, `title`, `ECTS`, `cycle`, `semester`, `subject_typ
 -- Table structure for table `users`
 --
 
-CREATE TABLE `users` (
-  `id` bigint(20) NOT NULL,
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   `surname` varchar(255) DEFAULT NULL,
   `JMBG` int(11) DEFAULT NULL,
@@ -177,98 +198,22 @@ CREATE TABLE `users` (
   `gender` varchar(255) DEFAULT NULL,
   `username` varchar(255) DEFAULT NULL,
   `password_digest` varchar(255) DEFAULT NULL,
-  `category` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `category` varchar(255) DEFAULT NULL,
+  `salary` double DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `surname`, `JMBG`, `date_of_birth`, `place_of_birth`, `phone`, `email`, `gender`, `username`, `password_digest`, `category`) VALUES
-(1, 'Amina', 'Mahmutovic', 1234567, '1995-11-03', 'Konjic', '062518676', 'amina@gmail.com', 'zensko', 'amina', '$2a$10$rViZ7Qmg7RzRsFeZQ9oMIewQHMnVLK6gJUQHxo6WXDMmPgx/nC//i', 'pravnik'),
-(2, 'Delila', 'Halilovic', 145673, '1995-12-01', 'Sarajevo', '062345189', 'delila@gmail.com', 'zensko', 'delila', '$2a$10$rViZ7Qmg7RzRsFeZQ9oMIewQHMnVLK6gJUQHxo6WXDMmPgx/nC//i', 'studentskasluzba'),
-(3, 'Selsebil', 'Catic', 6578123, '1996-12-07', 'Aman', '061218565', 'selsebil@gmail.com', 'zensko', 'selsebil', '$2a$10$rViZ7Qmg7RzRsFeZQ9oMIewQHMnVLK6gJUQHxo6WXDMmPgx/nC//i', 'student'),
-(4, 'Delila', NULL, 12345, '2017-12-28', 'Sarajevo', '62134456', 'delila@gmail.com', 'zensko', 'novo', '$2a$10$F6NKw//B3LQfH8zTLWt1wua5ohejVBKJmgqpWzgmRuh/WWo.S1/46', NULL);
+INSERT INTO `users` (`id`, `name`, `surname`, `JMBG`, `date_of_birth`, `place_of_birth`, `phone`, `email`, `gender`, `username`, `password_digest`, `category`, `salary`) VALUES
+(1, 'Amina', 'Mahmutovic', 1234567, '1995-11-03', 'Konjic', '062518676', 'amina@gmail.com', 'zensko', 'amina', '$2a$10$rViZ7Qmg7RzRsFeZQ9oMIewQHMnVLK6gJUQHxo6WXDMmPgx/nC//i', 'pravnik', NULL),
+(2, 'Delila', 'Halilovic', 145673, '1995-12-01', 'Sarajevo', '062345189', 'delila@gmail.com', 'zensko', 'delila', '$2a$10$rViZ7Qmg7RzRsFeZQ9oMIewQHMnVLK6gJUQHxo6WXDMmPgx/nC//i', 'studentskasluzba', 3000),
+(3, 'Selsebil', 'Catic', 6578123, '1996-12-07', 'Aman', '061218565', 'selsebil@gmail.com', 'zensko', 'selsebil', '$2a$10$rViZ7Qmg7RzRsFeZQ9oMIewQHMnVLK6gJUQHxo6WXDMmPgx/nC//i', 'student', NULL),
+(4, 'Delila', NULL, 12345, '2017-12-28', 'Sarajevo', '62134456', 'delila@gmail.com', 'zensko', 'novo', '$2a$10$F6NKw//B3LQfH8zTLWt1wua5ohejVBKJmgqpWzgmRuh/WWo.S1/46', NULL, NULL),
+(5, 'Huse', 'Fatkic', 12365478, '1944-01-03', 'Sarajevo', '061368965', 'huse@gmail.com', 'musko', 'huse', '$2a$10$rViZ7Qmg7RzRsFeZQ9oMIewQHMnVLK6gJUQHxo6WXDMmPgx/nC//i', 'pravnik', 4000);
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `ar_internal_metadata`
---
-ALTER TABLE `ar_internal_metadata`
-  ADD PRIMARY KEY (`key`);
-
---
--- Indexes for table `exams`
---
-ALTER TABLE `exams`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `index_exams_on_subject_id` (`subject_id`);
-
---
--- Indexes for table `exam_applications`
---
-ALTER TABLE `exam_applications`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `index_exam_applications_on_exam_id` (`exam_id`),
-  ADD KEY `index_exam_applications_on_student_id` (`student_id`);
-
---
--- Indexes for table `schema_migrations`
---
-ALTER TABLE `schema_migrations`
-  ADD PRIMARY KEY (`version`);
-
---
--- Indexes for table `students`
---
-ALTER TABLE `students`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `index_students_on_user_id` (`user_id`);
-
---
--- Indexes for table `subjects`
---
-ALTER TABLE `subjects`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `exams`
---
-ALTER TABLE `exams`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `exam_applications`
---
-ALTER TABLE `exam_applications`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `students`
---
-ALTER TABLE `students`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `subjects`
---
-ALTER TABLE `subjects`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- Constraints for dumped tables
 --
@@ -291,6 +236,7 @@ ALTER TABLE `exam_applications`
 --
 ALTER TABLE `students`
   ADD CONSTRAINT `fk_rails_148c9e88f4` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
